c22869e4c6cde83330df718fc037a7a0249feb54
shivam singh <133215640+llshivamsinghll@users.noreply.github.com>
2025-05-13 00:12:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ async def chat(msg: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=0000)
